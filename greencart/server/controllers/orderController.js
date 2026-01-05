b85285deba4a3779e4d2879759f2ca0a5dac3f4a
kundan kumar <dubeykundan351@gmail.com>
2026-01-05 22:12:24 +0530

    fixed the userId for order

--- greencart/server/controllers/orderController.js
+++ greencart/server/controllers/orderController.js
@@ -16,7 +16,8 @@ import stripe from 'stripe'
 //place order COD: /api/order/cod
 export const placeOrderCOD=async(req,res)=>{
   try {
-    const {userId,items,address}=req.body;
+    const userId=req.userId;
+    const {items,address}=req.body;
     if(!address || items.length===0){
       return res.json({success:false,message:"Invalid data"})
     }
